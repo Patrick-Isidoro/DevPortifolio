262bc328f80b219c79ddcfc35c697e65ac5f361b
unknown <patrickisidoro@gmail.com>
2018-04-09 13:18:17 -0300

    bug fixed..

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import {Navlink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 //Importando components necessários da lib react-materialize
 import { Navbar, NavItem, Row } from 'react-materialize'
 
 const Header = () => ( 
     <Row >
-        <Navbar className = 'grey darken-2' >
-            <li><Navlink href = '/' > Home </Navlink></li>  
-            <li><Navlink href = '/contact' > Contact </Navlink></li>
+        <Navbar className ='grey darken-2'>
+            <li><NavLink to = '/' > Home </NavLink></li>  
+            <li><NavLink to = '/contact' > Contact </NavLink></li>
         </Navbar > 
     </Row>
 )
